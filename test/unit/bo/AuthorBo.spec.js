7e4916a96d76ea7a5158693711c52090bcedb9e9
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-07-01 00:56:09 -0300

    Add tests method findById way success and failed in author

--- test/unit/bo/AuthorBo.spec.js
+++ test/unit/bo/AuthorBo.spec.js
@@ -37,4 +37,37 @@ describe("Suit tests AuthorBo", () => {
 
     });
 
+    it("Should return author specified", async () => {
+        const idFake = 1;
+        const authorFake = { 
+            name: "Author fake",
+            describe: "Master fake",
+            email: "authorfake@gmail.com",
+            password: "1234"
+        };
+        const daoFake = {
+            findById: sinon.stub()
+        };
+
+        daoFake.findById.withArgs(idFake).returns(authorFake);
+        const authorBO = new AuthorBO(daoFake);
+        const authorReturned = await authorBO.findById(idFake);
+        expect(authorReturned).to.be.eq(authorFake);
+    });
+
+    it("Should trigger exception when author not found", async () => {
+        const idFake = 1;
+        const daoFake = {
+            findById: sinon.stub()
+        };
+
+        daoFake.findById.withArgs(idFake).returns(null);
+        const authorBO = new AuthorBO(daoFake);
+        try {
+            const authorReturned = await authorBO.findById(idFake);
+        } catch(e) {
+            expect(e.code).to.be.eq("NOT_FOUND");
+        }
+    });
+
 });
