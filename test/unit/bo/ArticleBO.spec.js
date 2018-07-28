26b0a2842cb38bcef97eba6a499736cdc84c38c5
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-07-28 14:53:40 -0300

    Added test unit in method findById with way success and failed.

--- test/unit/bo/ArticleBO.spec.js
+++ test/unit/bo/ArticleBO.spec.js
@@ -6,6 +6,11 @@ const expect = chai.expect;
 
 describe("Suit tests ArticleBO", () => {
 
+    const daoFake = {
+        findAll: sinon.stub(),
+        findById: sinon.stub()
+    };
+
     afterEach(() => sinon.stub());
 
     it("Should return list articles", async () => {
@@ -29,13 +34,38 @@ describe("Suit tests ArticleBO", () => {
                 comments: ["Fake test"]
             }
         ];
-        const daoFake = {
-            findAll: sinon.stub()
-        };
 
         daoFake.findAll.withArgs().returns(articlesFake);
         const articleBO = new ArticleBO(daoFake);
         const articlesReturned = await articleBO.findAll();
         expect(articlesReturned.length).to.be.eq(articlesFake.length);
+    });
+
+    it("Should return article based id information", async () => {
+        const idFake = "56cb91bdc3464f14678934ba";
+        const articleFake = {
+            title: "Article fake to test",
+            content: "Article fake",
+            tags: ["Test", "Fake"],
+            comments: ["Fake test"]
+        };
+        daoFake.findById.withArgs(idFake).returns(articleFake);
+        const articleBO = new ArticleBO(daoFake);
+        const articleReturned = await articleBO.findById(idFake);
+        expect(articleReturned.title).to.eql(articleFake.title);
+        expect(articleReturned.tags).to.eql(articleFake.tags);
+        expect(articleReturned.content).to.eql(articleFake.content);        
+    });
+
+    it("Should trigger exception when not found register", async () => {
+        const idFake = "56cb91bdc3464f14678934ba";
+        daoFake.findById.withArgs(idFake).returns(null);
+        const articleBO = new ArticleBO(daoFake);
+        try {
+            await articleBO.findById(idFake);
+        } catch(e) {
+            expect(e.code).to.eq("NOT_FOUND");
+        }
+        
     });
 });
