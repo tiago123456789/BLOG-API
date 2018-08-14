a2c07c517c32ec03039f8081ed0f7935c2616fed
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-08-13 21:01:28 -0300

    Init implementation auth jwt in app

---
+++ src/security/Token.js
@@ -0,0 +1,33 @@
+export default class Token {
+
+    static TYPE = {
+        REFRESH: "REFRESH",
+        ACCESS: "ACCESS"
+    }
+
+    constructor(typeToken) {
+        this._payload = {};
+        this._secret = proccess.env.TOKEN_SECRET;
+        this._type = typeToken;
+    }
+
+    addValuePayload(key, value) {
+        this._payload[key] = value;
+    }
+
+    getPayload() {
+        return this._payload;
+    }
+
+    getSecret() {
+        return Buffer.from(this._secret).toString("base64")
+    }
+
+    getTimeExpired() {
+        if (this._type == Token.TYPE.ACCESS) {
+            return process.env.ACCESS_TOKEN_TIME_EXPIRED;
+        } else {
+            return process.env.REFRESH_TOKEN_TIME_EXPIRED;
+        }
+    }
+}
