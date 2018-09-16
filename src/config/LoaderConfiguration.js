b0ca311bcf029d9403f7de91d5e8eced9a4d52c7
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-09-16 11:42:12 -0300

    Resolve failed tests unit and intergration

--- src/config/LoaderConfiguration.js
+++ src/config/LoaderConfiguration.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 const getFileWithConfigurations = () => {
     let environment = process.env.NODE_ENV || "";
-    environment = environment.toUpperCase();
+    environment = environment.toLowerCase();
     return (environment.length == 0 ? "./.env": `./.env-${environment}`);
 }
 
